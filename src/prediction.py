e903d954bb85bed2a0c4e7ad5bc59922533b64e8
Christianib003 <christianib003@gmail.com>
2025-07-31 15:23:36 +0200

    refactor code into src package

--- src/prediction.py
+++ src/prediction.py
@@ -1,27 +1,17 @@
 import tensorflow as tf
 import numpy as np
+from src.preprocessing import preprocess_image
 
-# A list of output class names
 CLASS_NAMES = [
-    'Corn_(maize)___Common_rust_',
-    'Corn_(maize)___healthy',
-    'Potato___Early_blight',
-    'Potato___healthy',
-    'Tomato___Bacterial_spot',
-    'Tomato___healthy'
+    'Corn_(maize)___Common_rust_', 'Corn_(maize)___healthy', 
+    'Potato___Early_blight', 'Potato___healthy', 
+    'Tomato___Bacterial_spot', 'Tomato___healthy'
 ]
 
-model = tf.keras.models.load_model('../models/plant_classifier_v1.keras')
-
-def preprocess_image(image_bytes):
-    """Preprocesses the input image bytes for prediction."""
-    img = tf.image.decode_image(image_bytes, channels=3)
-    img = tf.image.resize(img, [224, 224])
-    img = tf.expand_dims(img, axis=0) 
-    return img
+model = tf.keras.models.load_model('models/plant_classifier_v1.keras')
 
 def predict_single(image_bytes):
-    """Makes a prediction on a single image and returns the class name and confidence."""
+    """Makes a prediction on a single image."""
     processed_image = preprocess_image(image_bytes)
     prediction = model.predict(processed_image)
     
